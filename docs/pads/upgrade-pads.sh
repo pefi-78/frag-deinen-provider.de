a2f8e980acc796b68e1c84c890b7e4ea7d05874e
peter fischer <s0539130@htw-berlin.de>
2019-05-31 19:34:51 +0200

    remove old pads and add pad-update-script

---
+++ docs/pads/upgrade-pads.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# 
+# Script zo save the wikipads
+
+BASE_URL=https://yourpart.eu/p/frag-deinen-provider
+PADS=("frag-deinen-provider" "frag-deinen-provider-mail-anfrage" "frag-deinen-provider-kampagne" "frag-deinen-provider-anfrage-text" "frag-deinen-provider-kontaktdaten" "frag-deinen-provider-datenschutzerlaerung")
+
+
+if [ -z "$(git status --porcelain $PWD)" ]; then 
+  echo "Working directory clean"
+else 
+  echo "Uncommitted changes in $PWD"
+  git status $PWD
+  exit 1
+fi
+
+
+for p in "${PADS[@]}"
+do	
+	cmd="wget '$BASE_URL/$p/export/markdown' -O '$p.md'"
+        #echo $cmd
+	$cmd
+        git add "$PWD/*"
+        git commit -m "Update pads"
+done
+
+exit 0
